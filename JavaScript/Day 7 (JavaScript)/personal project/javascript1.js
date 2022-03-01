031f29e6be4a84ca5125a5fa79373542333759bf
Bhalchandra Rane <brane6963@gmail.com>
2022-03-01 17:23:38 +0530

    added Javascript examples

---
+++ JavaScript/Day 7 (JavaScript)/personal project/javascript1.js
@@ -0,0 +1,67 @@
+document.querySelector('#check').addEventListener('click',check)
+function check(){
+    const day = document.querySelector('#day').value.toLowerCase()
+    //condition go here
+    if(day === 'monday'){
+        alert("Boring......");
+    }
+    else if(day === 'tuesday'){
+       alert("School P.E Day....");
+    }
+    else if(day === 'wednesday'){
+        alert("Hungry canteen Day...");
+    }
+    else if(day === 'thrusday'){
+        alert("Another Useless Boring day......");
+    }
+    else if(day === 'friday'){
+        alert("Friday is the go home early day");
+    }
+    else if(day === 'saturday' || day === 'sunday'){
+        alert("Finally its WEEKEND!!!!!!!!");
+    }
+    else{
+        alert("Invlid input !");
+    }
+}
+//Simplete calcultor code
+let total= 0
+document.querySelector('#zero').addEventListener('click',zero)
+document.querySelector('#three').addEventListener('click',three)
+document.querySelector('#nine').addEventListener('click',nine)
+document.querySelector('#two').addEventListener('click',two)
+
+function zero(){
+    total = 0
+    document.querySelector('#result').innerText = total
+}
+function three(){
+    total = total + 3
+    document.querySelector('#result').innerText = total
+}
+function nine(){
+    total = total + 9
+    document.querySelector('#result').innerText = total
+}
+function two(){
+    total = total -2
+    document.querySelector('#result').innerText = total
+}
+
+/*this below Program changes the Background */
+document.getElementById('purple').onclick = partyPurple
+document.getElementById('green').onclick = partyGreen
+document.getElementById('blue').onclick = partyBlue
+
+function partyPurple()  {
+    document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
+    document.querySelector('body').style.color ='white'
+}
+function partyGreen()   {
+    document.querySelector('body').style.backgroundColor = 'rgba(0,253,81,1)'
+    document.querySelector('body').style.color ='white'
+}
+function partyBlue()  {
+    document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
+    document.querySelector('body').style.color ='white'
+}
