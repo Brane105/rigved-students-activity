46efa4aba78be78367eae47fb2942727c1fce519
Bhalchandra Rane <brane6963@gmail.com>
2022-03-23 18:47:51 +0530

    Day 22

--- NodeJS/Day 22/ex2.js
+++
@@ -1,5 +0,0 @@
-let read = require("readline-sync");
-let id = read.questionInt("Enter your ID : ");
-let name = read.question("Enter your name : ");
-let salary = read.questionInt("Enter your Salary : ");
-console.log(`id : ${id}, name : ${name}, salary : ${salary}`);
