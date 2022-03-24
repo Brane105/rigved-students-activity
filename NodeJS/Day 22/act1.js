b7baf926d201a1c62bbdc7ce0a2474e4236493b8
Bhalchandra Rane <brane6963@gmail.com>
2022-03-24 11:54:31 +0530

    Updated Day 22

--- NodeJS/Day 22/act1.js
+++ NodeJS/Day 22/act1.js
@@ -1,18 +1,38 @@
+
 let read=require('readline-sync');
 let fs=require('fs');
+let data=fs.readFileSync('act1.json');
+let dataString = data.toString();
+let jsArray = undefined;
+if (dataString.length < 1 || dataString == ""){
+    jsArray = [];
+}
+else{
+    jsArray = JSON.parse(dataString)
+}
 let id=read.questionInt('Enter your id: ');
 let name=read.question('Enter your name: ');
 let salary=read.questionInt('Enter your salary: ');
-let employee={id:id,name:name,salary:salary};
-let jsonString=JSON.stringify(employee);
-let data=fs.readFileSync('act1.json').toString();
-if(data.endsWith('}]')==true){
-    let d1=data.slice(0,(data.length-1))
-    let d2=`${d1},\n${jsonString}]`
-    fs.writeFileSync('act1.json',d2);
-    console.log('successful...')
-}else{
-    let d2=`[${jsonString}]`
-    fs.writeFileSync('act1.json',d2);
-    console.log('successful...')
-}
+let emp = {id:id,name:name,salary,salary};
+jsArray.push(emp);
+let jsonArray = JSON.stringify(jsArray);
+fs.writeFileSync("act1.json",jsonArray);
+//let read=require('readline-sync');
+//let fs=require('fs');
+//let id=read.questionInt('Enter your id: ');
+//let name=read.question('Enter your name: ');
+//let salary=read.questionInt('Enter your salary: ');
+//let employee={id:id,name:name,salary:salary};
+//let jsonString=JSON.stringify(employee);
+//let data=fs.readFileSync('act1.json').toString();
+//if(data.endsWith('}]')==true){
+   // let d1=data.slice(0,(data.length-1))
+    //let d2=`${d1},\n${jsonString}]`
+   // fs.writeFileSync('act1.json',d2);
+   // console.log('successful...')
+//}else{
+  //  let d2=`[${jsonString}]`
+  //  fs.writeFileSync('act1.json',d2);
+   // console.log('successful...')
+//}
+
--- NodeJS/Day 23/ex1.js
+++ NodeJS/Day 23/ex1.js
@@ -1,8 +1,8 @@
 let fs=require('fs');
-let emp = fs.readFileSync('act1.json');
+let emp = fs.readFileSync('ex1.json');
 let jsonString=emp.toString();
 let jsobj=JSON.parse(jsonString);
 for(let i=0;i<jsobj.length;i++){
-    let{id, name, salary}=jsobj[i];
-    console.log(`Name: ${id}, Age: ${name}, Salary: ${salary}`)
+    let{id,name}=jsobj[i];
+    console.log(`id: ${id}, name: ${name}`)
 }
