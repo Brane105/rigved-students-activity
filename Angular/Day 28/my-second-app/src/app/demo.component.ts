2bf2c222c185684a6a572e65672216edb8938b0c
Bhalchandra Rane <brane6963@gmail.com>
2022-03-31 16:59:02 +0530

    Day 28

---
+++ Angular/Day 28/my-second-app/src/app/demo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector : 'demo',
+    templateUrl : './demo.component.html'
+})
+export class DemoComponent {
+    username = "Lucifer";
+    Age = 35;
+}
